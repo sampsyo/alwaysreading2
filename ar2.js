73f10049bb18a45ecfc1ddd48f11f8464d99621b
Adrian Sampson <adrian@radbox.org>
2010-11-24 17:53:59 -0800

    move collection view stuff to a separate file

--- ar2.js
+++ ar2.js
@@ -22,48 +22,6 @@ $(function() {
     window.documentList = new DocumentList;
     
     
-    // Generic list views.
-    
-    Backbone.CollectionView = Backbone.View.extend({
-        collection: null, // The collection being displayed.
-        itemView: null, // A view for displaying a single item in the list.
-        
-        subviews: [],
-        selectedView: null,
-        initialize: function() {
-            _.bindAll(this, 'addItem');
-            this.collection.bind("add", this.addItem);
-        },
-        render: function() {
-            this.collection.each(this.addItem);
-            return this;
-        },
-        
-        addItem: function(model) {
-            var view = new (this.itemView)({'model': model});
-            $(this.el).append(view.render().el);
-            this.subviews[model.id] = view;
-        },
-        setSelection: function(id) {
-            if (this.selectedView) {
-                this.selectedView.setSelected(false);
-            }
-            if (id) {
-                this.selectedView = this.subviews[id];
-                this.selectedView.setSelected(true);
-            } else {
-                this.selectedView = null;
-            }
-        },
-    });
-    
-    Backbone.CollectionItemView = Backbone.View.extend({
-        setSelected: function(selected) {
-            // Implement this!
-        }
-    });
-    
-    
     // Document list views.
     
     window.DocumentItemView = Backbone.CollectionItemView.extend({
