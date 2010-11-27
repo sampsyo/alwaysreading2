eec71f5c2552db539a5d26c6ac20a9943d7925e3
Adrian Sampson <adrian@radbox.org>
2010-11-27 14:42:20 -0800

    move everything into a "static" directory

--- compile.sh
+++ compile.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
+indir=static
 outdir=min
 jsfiles="lib/underscore.js lib/json2.js lib/backbone.js lib/backbone-localstorage.js backbone-collectionview.js ar2.js"
-jsout=$outdir/ar2.js
+jsout=ar2.js
 cssin=ar2.css
-cssout=$outdir/ar2.css
+cssout=ar2.css
+htmlin=index.html
+htmlout=index.html
 
 mkdir -p $outdir
 
+# JavaScript.
 jsinargs=
 for jsfile in $jsfiles
 do
-    jsinargs="$jsinargs --js $jsfile"
+    jsinargs="$jsinargs --js $indir/$jsfile"
 done
-java -jar compiler.jar $jsinargs --js_output_file $jsout
+java -jar compiler.jar $jsinargs --js_output_file $outdir/$jsout
 
-java -jar yuicompressor.jar $cssin > $cssout
+# CSS.
+java -jar yuicompressor.jar $indir/$cssin > $outdir/$cssout
+
+# HTML.
+cp $indir/$htmlin $outdir/$htmlout
